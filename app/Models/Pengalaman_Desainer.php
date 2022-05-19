54c19ea1ae78c78aa624b1d61c3dc052e59b796d
TeamScrumbB1 <teamscrumb1@gmail.com>
2022-05-19 17:17:40 +0700

    penambahan model

---
+++ app/Models/Pengalaman_Desainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengalaman_Desainer extends Model
+{
+    protected $guarded = [];
+    protected $table ='pengalaman_desainer';
+    use HasFactory;
+}
